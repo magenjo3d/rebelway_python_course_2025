845640e6d0537970acd8125e91b9c932cc5791da
magenjo3d <m.agenjo3d@gmail.com>
2025-08-29 02:43:52 +0200

    publish button created

--- week3_assignment_publisher.py
+++ week3_assignment_publisher.py
@@ -1,5 +1,31 @@
 ## Asignment week 3 - git_asset-publisher_drive
 
 import hou
+import os
 
-#all the code here
+class Publisher:
+    def __init__(self):
+
+        self.name = None
+        self.version = None
+        self.path = None
+        self.user = None
+    
+    def set_publish_path(self): # 2) Func to get the string from the search field
+    
+        my_hda = hou.pwd()
+        
+        self.user = str(os.environ["USER"])
+        self.name = str(my_hda.parm("basename").eval())
+        self.version = str(my_hda.parm("version").eval())
+        
+        constructed_path = "$HIP/houdini/geo" + "/v" + self.version + "/" + self.user + "/" + self.name 
+        
+        my_hda.parm("file").set(constructed_path)
+        
+        self.path = str(my_hda.parm("file").eval())
+        my_hda.parm("execute").pressButton()
+        
+        print(self.path)
+        
+        
